4d0f67afe3049095664e9b8c2945035b028f8420
Lee Gao <leeyuan123@yahoo.com>
2012-03-09 20:45:14 -0500

    2

--- note/views.py
+++ note/views.py
@@ -1,8 +1,12 @@
 # Create your views here.
+from django.shortcuts import render_to_response
 
 from django.http import HttpResponse
 from models import *
 import json
+
+def index(request):
+    return render_to_response('template.html')
 
 def get(request, uid):
     # return the full text in json
--- urls.py
+++ urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     url(r'^get/(?P<uid>\d+)/$', 'note.views.get'),
     url(r'^update/(?P<uid>\d+)/(?P<pid>\d+)/$', 'note.views.update'),
     url(r'^poll/(?P<uid>\d+)/(?P<pid>\d+)/$', 'note.views.poll'),
+    url(r'^index','note.views.index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
