cfbcb6b06d65dbd6a8b12d3d484858bbdbf9cc54
unknown <Detian@Rockman.(none)>
2012-03-10 10:13:58 -0500

    omnipad

---
+++ notepad/note/admin.py
@@ -0,0 +1,7 @@
+__author__ = 'Lee'
+
+from models import Document, Cell
+from django.contrib import admin
+
+admin.site.register(Document)
+admin.site.register(Cell)
---
+++ notepad/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+import os
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'notepad.views.home', name='home'),
+    # url(r'^notepad/', include('notepad.foo.urls')),
+
+    url(r'^get/(?P<uid>\d+)/$', 'note.views.get'),
+    url(r'^update/(?P<uid>\d+)/(?P<pid>\d+)/$', 'note.views.update'),
+    url(r'^poll/(?P<uid>\d+)/(?P<pid>\d+)/$', 'note.views.poll'),
+    url(r'^index','note.views.index'),
+    url(r'^(?P<uid>\d+)/$','note.views.page'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.views.static',
+    (r'^static/(?P<path>.*)$',
+     'serve', {
+        'document_root': os.path.abspath('./static/'),
+        'show_indexes': True }),)
